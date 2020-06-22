0a4c51cefa2a0b6386c9c07d6118f89b0db38b5d
ArtStyle111 <49519304+ArtStyle111@users.noreply.github.com>
2020-06-22 18:32:49 +0300

    123

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -19,18 +19,59 @@ $(document).ready(function(){
         slidesToShow: 1,
         dots: false,
         autoplay: true
-    });
-    $('.fade').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-      });
-          
+    });   
     $('.header__burger').click(function(event) {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $(window).scroll(function() {
+        $('.down').each(function(){
+            var imagePos = $(this).offset().top;
+    
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInDown");
+            }
+        });
+    });
+    $(window).scroll(function() {
+        $('.up').each(function(){
+            var imagePos = $(this).offset().top;
+    
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }
+        });
+    });
+    $(window).scroll(function() {
+        $('.left').each(function(){
+            var imagePos = $(this).offset().top;
+    
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInLeftBig");
+            }
+        });
+    });
+    $(window).scroll(function() {
+        $('.right').each(function(){
+            var imagePos = $(this).offset().top;
+    
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInRight");
+            }
+        });
+    });
+    $(window).scroll(function() {
+        $('.bounce').each(function(){
+            var imagePos = $(this).offset().top;
+    
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    });
 });
-
