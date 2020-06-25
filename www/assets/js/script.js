97c6e33a215580306ebd7ffc825981c5cad91429
ArtStyle111 <49519304+ArtStyle111@users.noreply.github.com>
2020-06-25 15:18:12 +0300

    123
    123

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('.slider').slick({
+$(document).ready(function () {
+    $(".slider").slick({
         slidesToShow: 1,
         initialSlide: 1,
         arrows: true,
@@ -8,68 +8,67 @@ $(document).ready(function(){
             {
                 breakpoint: 576,
                 settings: {
-                    centerPadding: '0' 
-                }
-            }
-        ]
-         
-    });  
-    $('.slider__bg').slick({
+                    centerPadding: "0",
+                },
+            },
+        ],
+    });
+    $(".slider__bg").slick({
         arrows: true,
         slidesToShow: 1,
         dots: false,
-        autoplay: true
-    });   
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
+        autoplay: true,
     });
-    $(window).scroll(function() {
-        $('.down').each(function(){
+    $(".header__burger").click(function (event) {
+        $(".header__burger,.header__menu").toggleClass("active");
+        $("body").toggleClass("lock");
+    });
+    $(window).scroll(function () {
+        $(".down").each(function () {
             var imagePos = $(this).offset().top;
-    
+
             var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+650) {
+            if (imagePos < topOfWindow + 650) {
                 $(this).addClass("fadeInDown");
             }
         });
     });
-    $(window).scroll(function() {
-        $('.up').each(function(){
+    $(window).scroll(function () {
+        $(".up").each(function () {
             var imagePos = $(this).offset().top;
-    
+
             var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+650) {
+            if (imagePos < topOfWindow + 650) {
                 $(this).addClass("fadeInUp");
             }
         });
     });
-    $(window).scroll(function() {
-        $('.left').each(function(){
+    $(window).scroll(function () {
+        $(".left").each(function () {
             var imagePos = $(this).offset().top;
-    
+
             var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+650) {
+            if (imagePos < topOfWindow + 650) {
                 $(this).addClass("fadeInLeftBig");
             }
         });
     });
-    $(window).scroll(function() {
-        $('.right').each(function(){
+    $(window).scroll(function () {
+        $(".right").each(function () {
             var imagePos = $(this).offset().top;
-    
+
             var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+650) {
+            if (imagePos < topOfWindow + 650) {
                 $(this).addClass("fadeInRight");
             }
         });
     });
-    $(window).scroll(function() {
-        $('.bounce').each(function(){
+    $(window).scroll(function () {
+        $(".bounce").each(function () {
             var imagePos = $(this).offset().top;
-    
+
             var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+650) {
+            if (imagePos < topOfWindow + 650) {
                 $(this).addClass("fadeIn");
             }
         });
--- www/assets/php/send.php
+++ www/assets/php/send.php
@@ -1,15 +1,15 @@
 <?php if (isset($_POST["btn"])) {
-$to = "vladimir.orlof@gmail.com";
-$subject = "Письмо с сайта";
-$charset = "utf-8";
-$headerss ="Content-type: text/html; charset=$charset\r\n";
-$headerss.="MIME-Version: 1.0\r\n";
-$headerss.="Date: ".date('D, d M Y h:i:s O')."\r\n";
-$msg = "Name: ".$_POST["name"]."\n";
-$msg .= "E-mail: ".$_POST["email"]."\n";
-$msg .= "Phone: ".$_POST["phone"]."\n";
-$msg .= "Address: ".$_POST["adderess"]."\n";
-$msg .= "Message: ".$_POST["msg"]."\n";
-mail($to, $subject, $msg, $headerss);
-print "<script>alert(\"Сообщение успешно отправлено!\");window.location = window.location.href</script>";
+    $to = "vladimir.orlof@gmail.com";
+    $subject = "Письмо с сайта";
+    $charset = "utf-8";
+    $headerss = "Content-type: text/html; charset=$charset\r\n";
+    $headerss .= "MIME-Version: 1.0\r\n";
+    $headerss .= "Date: " . date('D, d M Y h:i:s O') . "\r\n";
+    $msg = "Name: " . $_POST["name"] . "\n";
+    $msg .= "E-mail: " . $_POST["email"] . "\n";
+    $msg .= "Phone: " . $_POST["phone"] . "\n";
+    $msg .= "Address: " . $_POST["adderess"] . "\n";
+    $msg .= "Message: " . $_POST["msg"] . "\n";
+    mail($to, $subject, $msg, $headerss);
+    print "<script>alert(\"Сообщение успешно отправлено!\");window.location = window.location.href</script>";
 }
