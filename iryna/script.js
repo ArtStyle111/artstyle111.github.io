4670bff7540014d64bd16d651e5ec11b1a211072
ArtStyle111 <49519304+ArtStyle111@users.noreply.github.com>
2020-08-30 18:45:12 +0300

    iryna

---
+++ iryna/script.js
@@ -0,0 +1,3 @@
+//Check out more cool stuff on my site: http://f-rilling.com/
+//if some fonts are not being loaded, reload the page
+//all fonts hosted on google fonts: https://www.google.com/fonts
