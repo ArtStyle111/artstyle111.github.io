f0d93b7f8e8dc0e59a779bfe8da323d881b5e481
ArtStyle111 <49519304+ArtStyle111@users.noreply.github.com>
2021-06-19 17:17:34 +0300

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,3 @@
 document.getElementById('card').onclick = function() {
   document.getElementById('card').classList.toggle('card-open');
 };
-document.getElementById('body').onclick = function() {
-  document.getElementById('card').classList.remove('card-open');
-};
