2e8da53c120f8fdf2842513375af3326b2c1e3f9
ArtStyle111 <49519304+ArtStyle111@users.noreply.github.com>
2021-06-19 15:20:19 +0300

    df

--- dist/script.js
+++ dist/script.js
@@ -2,3 +2,6 @@
 document.getElementById('card').onclick = function() {
   document.getElementById('card').classList.toggle('card-open');
 };
+document.getElementById('card').onclick = function() {
+  document.getElementById('card').classList.remove('card-open');
+};
