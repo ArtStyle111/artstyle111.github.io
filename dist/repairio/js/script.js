20867b7d152eb77dfaedf73011b3ea9b78477f4d
ArtStyle111 <49519304+ArtStyle111@users.noreply.github.com>
2021-09-16 05:50:27 -0400

    asdf

---
+++ dist/repairio/js/script.js
@@ -0,0 +1,8 @@
+bulmaCarousel.attach('#trend-slide', {
+			slidesToScroll: 1,
+			slidesToShow: 6,
+      pagination: false,
+      loop: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+		});
